7e576ea2418b7a7996507d855c657dc814d958f1
TheParotta <126277487+theparotta@users.noreply.github.com>
2023-12-02 20:59:37 +0530

    Cylon Eye added

---
+++ cylon_eyes/cylon_eyes.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DELAY 100             /* 2 milliseconds */
+
+int main() {
+  /* setting all the pins in BANK-D as output */
+  DDRD = 0xFF;
+  
+  uint8_t loop = 0;
+  
+  while (1) {
+    
+    /* moving forward */
+    while (loop < 7) {
+      PORTD = (1 << loop);     /* moving '1' bit based on loops value */
+      _delay_ms(DELAY);
+      loop++;
+    }
+    
+    /* moving backward */
+    while (loop > 0) {
+      PORTD = (1 << loop);
+      _delay_ms(DELAY);
+      loop--;
+    }
+  }
+  
+  return(0);
+}
--- pov_toy/pov_toy.c
+++ pov_toy/pov_toy.c
@@ -1,8 +1,35 @@
 #include <avr/io.h> 
 #include <util/delay.h>
 
+void blink_leds(uint8_t);
+
 int main() {
+  // setting all the pins in BANK-B as output
+  DDRB = 0xFF;
   
+  while (1) {
+    // turning on/off LEDS in below pattern
+    blink_leds(0b00001110);
+    blink_leds(0b00011000);
+    blink_leds(0b10111101);
+    blink_leds(0b01110110);
+    blink_leds(0b00111100);
+    blink_leds(0b00111100);
+    blink_leds(0b00111100);
+    blink_leds(0b01110110);
+    blink_leds(0b10111101);
+    blink_leds(0b00011000);
+    blink_leds(0b00001110);
+    
+    PORTB = 0;
+    _delay_ms(10);
+  }
   
   return(0);
+}
+
+
+void blink_leds(uint8_t portb) {
+  PORTB = portb;
+  _delay_ms(2);  
 }
